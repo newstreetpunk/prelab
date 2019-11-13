160a02af626d3e37361d63974c1699c0326cc0b7
newstreetpunk <newstreetpunk@yandex.ru>
2019-11-14 00:07:31 +0500

    Services

--- js/common.js
+++ js/common.js
@@ -6,4 +6,13 @@ $(function() {
 		$('.logo').slideToggle(40);
 	});
 
+	var $page = $('html, body');
+
+	$('.form-open').on('click', function(){
+		$(this).hide(50).prev().slideDown(70);
+		$page.animate({
+            scrollTop: $('.callback').offset().top - 80}, 50);
+		return false;
+	});
+
 });
