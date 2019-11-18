47dfdca2d7cc0c02cb72610fa104a1777036f2f5
newstreetpunk <newstreetpunk@yandex.ru>
2019-11-18 18:24:33 +0500

    345

--- js/common.js
+++ js/common.js
@@ -3,13 +3,13 @@ $(function() {
 	$('.mobile-btn').on('click', function() {
 		$('.menu').slideToggle(50);
 		$(this).toggleClass('active');
-		$('.logo').slideToggle(40);
+		// $('.logo').slideToggle(40);
 	});
 
 	var $page = $('html, body');
 
 	$('.form-open').on('click', function(){
-		$(this).hide(50).prev().slideDown(70);
+		$(this).hide(50).next().slideDown(70);
 		$page.animate({
             scrollTop: $('.callback').offset().top - 80}, 50);
 		return false;
